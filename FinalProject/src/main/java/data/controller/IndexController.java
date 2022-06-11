29c59d205097f5a6d73d64c441130b33bebae7d3
Leedasol2 <dasol2267@naver.com>
2022-06-11 17:01:20 +0900

    [이다솔] tiles 세팅

--- FinalProject/src/main/java/data/controller/IndexController.java
+++ FinalProject/src/main/java/data/controller/IndexController.java
@@ -5,13 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/html")
 public class IndexController {
 
 	@GetMapping("/")
 	public String home() {
 		
-		return "main";
+		return "/layout/main";
 	}
 	
 	
