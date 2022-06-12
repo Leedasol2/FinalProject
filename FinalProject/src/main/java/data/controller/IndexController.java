c5d3085cbd1069d842c7226990de50d1fb7987bd
LAPTOP-162OK0SK\pony4 <pony4021@hanmail.net>
2022-06-12 19:55:23 +0900

    [이강진] 컨트롤러 매핑

--- FinalProject/src/main/java/data/controller/IndexController.java
+++ FinalProject/src/main/java/data/controller/IndexController.java
@@ -1,12 +1,9 @@
 package data.controller;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 @Controller
 public class IndexController {
-
 	@GetMapping("/")
 	public String home() {
 		
@@ -18,9 +15,66 @@ public class IndexController {
 		
 		return "/html/festival";
 	}
+
+	@GetMapping("/html/festivalCalendar")
+	public String festivalCalendar() {
+
+		return "/html/festivalCalendar";
+	}
+
+	@GetMapping("/html/myPageEdit")
+	public String myPageEdit() {
+
+		return "/html/myPageEdit";
+	}
+
+	@GetMapping("/html/myPageReview")
+	public String myPageReview() {
+
+		return "/html/myPageReview";
+	}
+
+	@GetMapping("/html/myPageScrap")
+	public String myPageScrap() {
+
+		return "/html/myPageScrap";
+	}
+
+	@GetMapping("/html/myPageCalendar")
+	public String myPageCalendar() {
+
+		return "/html/myPageCalendar";
+	}
+
+	@GetMapping("/html/themePark")
+	public String themePark() {
+
+		return "/html/themePark";
+	}
+
+	@GetMapping("/html/modalCalendar")
+	public String modalCalendar() {
+
+		return "/html/modalCalendar";
+	}
 	
 	
+	@GetMapping("html/mytripdetail")
+	public String mytripdetail() {
+		
+		return "/html/myTripDetail";
+	}
 	
+	@GetMapping("html/festivaldetail")
+	public String festivaldetail() {
+		
+		return "/html/festivalDetail";
+	}
 	
+	@GetMapping("html/themeparkdetail")
+	public String themeparkdetail() {
+		
+		return "/html/themeParkDetail";
+	}
 	
 }
