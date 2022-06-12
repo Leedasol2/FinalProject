7f089f075f314be51d9d26a804eb77108de3c851
yongki1403 <dydrlwja55@naver.com>
2022-06-12 19:48:54 +0900

    [이용기] 컨트롤러 매핑 설정

--- FinalProject/src/main/java/data/controller/IndexController.java
+++ FinalProject/src/main/java/data/controller/IndexController.java
@@ -19,6 +19,48 @@ public class IndexController {
 		return "/html/festival";
 	}
 	
+	@GetMapping("/html/festivalCalendar")
+	public String festivalCalendar() {
+		
+		return "/html/festivalCalendar";
+	}
+	
+	@GetMapping("/html/myPageEdit")
+	public String myPageEdit() {
+		
+		return "/html/myPageEdit";
+	}
+	
+	@GetMapping("/html/myPageReview")
+	public String myPageReview() {
+		
+		return "/html/myPageReview";
+	}
+	
+	@GetMapping("/html/myPageScrap")
+	public String myPageScrap() {
+		
+		return "/html/myPageScrap";
+	}
+	
+	@GetMapping("/html/myPageCalendar")
+	public String myPageCalendar() {
+		
+		return "/html/myPageCalendar";
+	}
+	
+	@GetMapping("/html/themePark")
+	public String themePark() {
+		
+		return "/html/themePark";
+	}
+	
+	@GetMapping("/html/modalCalendar")
+	public String modalCalendar() {
+		
+		return "/html/modalCalendar";
+	}
+	
 	
 	
 	
