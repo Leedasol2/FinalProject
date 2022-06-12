e72b9e1cd30876047f62706ffc66331ccb8d1f1e
yongki1403 <dydrlwja55@naver.com>
2022-06-12 17:41:11 +0900

    [이용기] 컨트롤러 매핑

--- FinalProject/src/main/java/data/controller/IndexController.java
+++ FinalProject/src/main/java/data/controller/IndexController.java
@@ -7,10 +7,52 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
-	@GetMapping("/")
-	public String home() {
+	@GetMapping("/html/festival")
+	public String festival() {
 		
-		return "/layout/main";
+		return "/html/festival";
+	}
+	
+	@GetMapping("/html/festivalCalendar")
+	public String festivalCalendar() {
+		
+		return "/html/festivalCalendar";
+	}
+	
+	@GetMapping("/html/myPageCalendar")
+	public String myPageCalendar() {
+		
+		return "/html/myPageCalendar";
+	}
+	
+	@GetMapping("/html/modalCalendar")
+	public String modalCalendar() {
+		
+		return "/html/modalCalendar";
+	}
+	
+	@GetMapping("/html/myPageEdit")
+	public String myPageEdit() {
+		
+		return "/html/myPageEdit";
+	}
+	
+	@GetMapping("/html/myPageReview")
+	public String myPageReview() {
+		
+		return "/html/myPageReview";
+	}
+	
+	@GetMapping("/html/myPageScrap")
+	public String myPageScrap() {
+		
+		return "/html/myPageScrap";
+	}
+	
+	@GetMapping("/html/themePark")
+	public String themePark() {
+		
+		return "/html/themePark";
 	}
 	
 	
