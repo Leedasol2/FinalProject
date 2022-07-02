7ece4b975acb714be050b532eacac31342106258
Leedasol2 <dasol2267@naver.com>
2022-07-03 00:59:02 +0900

    [이다솔] js 함수 오류 해결

--- FinalProject/src/main/webapp/js/boardupdateform.js
+++ FinalProject/src/main/webapp/js/boardupdateform.js
@@ -1,26 +1,7 @@
 $(document).ready(function() {
 
-	
-	$("span.photoimg").click(function(){
-		
-		$("#upload").trigger("click");
-	});
-	
-    $("input#upload").change(function () {
-        
-    	//alert("test");
-    	
-        var fileInput = document.getElementById("upload");
-          
-        var files = fileInput.files;
-        var fileCnt=files.length;
-       	//alert(fileCnt);
-        $("span#filenum").text("총 "+fileCnt+"개의 파일을 선택하셨습니다.");
-    });
-	
-	
 });
-function goBack() {
+function goBackUpdate() {
 	var a=confirm("수정을 취소하시겠습니까?")
 	if(a)
 		history.back();
