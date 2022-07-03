e01a7cefff6e30e3d58c1fca1a064a4454434bd0
kimeunbi0924 <204924@naver.com>
2022-07-03 18:50:39 +0900

    [김은비] 테마파크 상세페이지 js 수정

--- FinalProject/src/main/webapp/WEB-INF/myTrip/themeParkDetail.jsp
+++ FinalProject/src/main/webapp/WEB-INF/myTrip/themeParkDetail.jsp
@@ -12,12 +12,13 @@
 
 </head>
 <body>
-
 <script src="${root}/js/themeParkDetail.js"></script>
 
 
 	<!-- main 시작 -->
-	<input type="hidden" id="tnum" value="${tdto.tnum }">
+	<input type="hidden" id="tnum" name="tnum" value="${tdto.tnum }">
+	<input type="hidden" name="loginok" value="${sessionScope.loginok}">
+	<input type="hidden" name="myid" value="${sessionScope.myid}">
 	<div class="mytripdetail">
 	<div class="tripdetailsubject">
 	<div class="mainmenutitle">
@@ -62,7 +63,11 @@
 	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 	<img alt="" src="${root }/image/asset/링크.png"><a href="${tdto.link }"><span class="tripscrap"> 홈페이지</span></a>
 	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-	<img alt="" src="${root }/image/asset/스크랩안함.png" class="scrapImg"><span class="tripscrap"> 스크랩 하기</span>
+	<!-- 로그인하면 스크랩기능 활성화 -->
+	<c:if test="${not empty sessionScope.loginok || not empty sessionScope.loggedIn}">
+	<img alt="" src="${root }/image/asset/스크랩안함.png" class="scrapImg" id="scrapImg">
+	<span class="tripscrap" onclick="toggle_object(1); scrapEvent();"> 스크랩</span>
+	</c:if>
 	</div>
 	</div>
 	</div>
--- FinalProject/src/main/webapp/js/themeParkDetail.js
+++ FinalProject/src/main/webapp/js/themeParkDetail.js
@@ -1,5 +1,4 @@
-$(document).ready(function(){
-
+$(document).ready(function(){	
 	
 	loginok=$('input[name=loginok]').val();
 	myid=$('input[name=myid]').val();
@@ -7,104 +6,14 @@ $(document).ready(function(){
 	
 	rlist();
 	tmrcount();
-
 	
 	<!-- image 클릭 이벤트 -->
 	$(".small3").click(function(){
-		
 		var src=$(this).attr("src");
-		
 		$(".tripimage").attr("src",src);
 	});
-	
-	
-	//insert
-	$("#rbtn").click(function(){
-		var tnum=$(':hidden#tnum').val();
-		var rstar = $('input[name="rating"]:checked').val();
-		var rcontents = $('#detailcontent').val();
-		//alert("tnum:"+tnum+"rstar:"+rstar+"rcontents:"+rcontents);
-		//alert(myid);
-		$.ajax({
-			
-			type:"post",
-			dataType:"text",
-			url:"/themeParkDetail/rinsert",
-			data:{"tnum":tnum,
-			"rstar":rstar,
-			"rcontents":rcontents},
-			success:function(data){
-				
-				$('#detailcontent').val("");
-				$('input[name="rating"]').prop("checked",false);
-				window.location.reload();
-			}
-		});
-		
-	});
-	
-	//delete
-	$(document).on("click","button.rdel",function(){
-		var rnum=$(this).attr("rnum");
-		//alert(rnum);
-		var a=confirm("해당 리뷰를 삭제할까요?");
-		
-		if(a){
-			$.ajax({
-				
-				type:"get",
-				dataType:"text", //return값 없으니까 text
-				url:"/themeParkDetail/rdelete",
-				data:{"rnum":rnum},
-				success:function(data){
-					$('#myModal3 [data-dismiss]').click();	
-					window.location.reload();
-				}
-			});	
-		}
-	});
-	
-	
-	//update
-	$(document).on("click","button.rmod",function(){
 
-		var rnum=$(this).attr("rnum");
-		
-		$.ajax({
-			
-			type:"get",
-			dataType:"json",
-			url:"/themeParkDetail/rdata",
-			data:{"rnum":rnum},
-			success:function(data){
-				//모달에 idx값을 가진 content띄우기
-				$("#ucontent").val(data.rcontents);
-			}
-		});
-		
-		$("#myModal3").modal();
-	});
-	
-	
-$(document).on("click","#btnaupdate",function(){
-		
-		var rnum=$("button.rmod").attr("rnum");
-		var rcontents=$("#ucontent").val();
-		
-		$.ajax({
-			
-			type:"post",
-			dataType:"text",
-			url:"/themeParkDetail/rupdate",
-			data:{"rnum":rnum,"rcontents":rcontents},
-			success:function(data){
-				window.location.reload();
-			}
-		});
-	});
-	
 	$("span.tripscrap").click(function(){
-		var tnum=$(':hidden#tnum').val();
 		$("img.scrapImg").attr("src","../image/asset/스크랩아이콘.png");
 		$.ajax({
 			
@@ -116,54 +25,90 @@ $(document).on("click","#btnaupdate",function(){
 				
 			}
 		});
-	});			
-});	
-
-function maskingCar(userid) {
-    if (userid == undefined || userid === '') {
-        return '';
-    }
-    var pattern = /.{3}$/; // 정규식
-    return userid.replace(pattern, "***");
-}
-
-function tmrcount()
-{
-	
-	var tnum=$(':hidden#tnum').val();
-	var myid="${sessionScope.myid}";
-	
-	$.ajax({
-		type:"get",
-		dataType:"text",
-		url:"/mypage/tmrcount",
-		data:{"tnum":tnum,"myid":myid},
-		success:function(data){
-			$(".btnadd").click(function(){
-				if(data>=1){
-					alert("리뷰는 한 ID 당 1개씩 작성할 수 있습니다.")
-					$(this).removeAttr("data-target");
-					$(this).removeAttr("data-toggle");
-				}
-			});
-	}
 	});
-}
+	
+	//review insert
+	$("#rbtn").click(function(){
+		var rstar = $('input[name="rating"]:checked').val();
+		var rcontents = $('#detailcontent').val();
+		$.ajax({
+			type:"post",
+			dataType:"text",
+			url:"/themeParkDetail/rinsert",
+			data:{"tnum":tnum,
+			"rstar":rstar,
+			"rcontents":rcontents},
+			success:function(data)
+			{
+				$('#detailcontent').val("");
+				$('input[name="rating"]').prop("checked",false);
+				window.location.reload();
+			}
+		});
+	});
+	
+	//review delete
+	$(document).on("click","button.rdel",function(){
+		var rnum=$(this).attr("rnum");
+		var a=confirm("해당 리뷰를 삭제할까요?");
+		
+		if(a){
+			$.ajax({
+				type:"get",
+				dataType:"text",
+				url:"/themeParkDetail/rdelete",
+				data:{"rnum":rnum},
+				success:function(data){
+					$('#myModal3 [data-dismiss]').click();	
+					window.location.reload();
+				}
+			});	
+		}
+	});
+	
+	//review get
+	$(document).on("click","button.rmod",function(){
+		var rnum=$(this).attr("rnum");
+		
+		$.ajax({
+			type:"get",
+			dataType:"json",
+			url:"/themeParkDetail/rdata",
+			data:{"rnum":rnum},
+			success:function(data){
+				$("#ucontent").val(data.rcontents);
+			}
+		});
+		$("#myModal3").modal();
+	});
+	
+	//review update
+	$(document).on("click","#btnaupdate",function(){
+		var rnum=$("button.rmod").attr("rnum");
+		var rcontents=$("#ucontent").val();
+		
+		$.ajax({
+			type:"post",
+			dataType:"text",
+			url:"/themeParkDetail/rupdate",
+			data:{"rnum":rnum,"rcontents":rcontents},
+			success:function(data){
+				window.location.reload();
+			}
+		});
+	});
+});
 
-
+//review list
 function rlist()
 {
-	var tnum=$(':hidden#tnum').val();
-	
 	$.ajax({
 		type:"get",
 		dataType:"json",
 		url:"/themeParkDetail/rlist",
 		data:{"tnum":tnum},
 		success:function(data){
-			 //console.log(data);
 			var r="";
-			
 			$.each(data,function(i,drdto){
 				r+="<div class='review-contents'>";
 				r+="<div class='review-topbox'>";
@@ -188,12 +133,12 @@ function rlist()
 				r+="</div>";
 				r+="</div>";
 				
-				if (loginok=="yes" && myid==drdto.userid) {
+				if (loginok=="yes" && myid==drdto.userid) 
+				{
 					r+="<div class='review-edit'>";
 					r+="<button type='button' class='rdel' rnum='"+drdto.rnum+"'>삭제</button>";
 					r+="<button type='button' class='rmod' rnum='"+drdto.rnum+"'>수정</button>";
 				}
-				
 				
 				r+="<div class='review-day'>";
 				r+="<span>"+drdto.rday+"</span>";
@@ -213,4 +158,63 @@ function rlist()
 			$("div.detailreview").html(r);
 		}
 	});
+}
+
+//review Duplicate check
+function tmrcount()
+{
+	$.ajax({
+		type:"get",
+		dataType:"text",
+		url:"/mypage/tmrcount",
+		data:{"tnum":tnum,"myid":myid},
+		success:function(data){
+			$(".btnadd").click(function(){
+				if(data>=1){
+					alert("리뷰는 한 ID 당 1개씩 작성할 수 있습니다.")
+					$(this).removeAttr("data-target");
+					$(this).removeAttr("data-toggle");
+				}
+			});
+		}
+	});
+}
+
+//review id masking
+function maskingCar(userid) {
+    if (userid == undefined || userid === '') {
+        return '';
+    }
+    var pattern = /.{3}$/; // 정규식
+    return userid.replace(pattern, "***");
+}
+//scrap
+function scrapEvent(){
+	$.ajax({
+		type:"post",
+		dataType: "text",
+		url:"/scrapInsert",
+		data:{"tnum":tnum},
+		success:function(data){
+			
+		}
+	});
+}
+
+//scrap icon change
+var i = 0;
+function toggle_object(post_id)
+{  
+    i = i + post_id;
+    var obj = document.getElementById('scrapImg');  
+    if(!obj) return;  
+ 
+    if(i%2!=0)
+    {  
+        obj.src="../image/asset/스크랩아이콘.png";
+    }
+    else
+    {  
+        obj.src="../image/asset/스크랩안함.png";
+    }
 }
